dc9f9e348fdc49b2c07d59edb9b0ae64bbb20a5f
Your Name <you@example.com>
2022-08-04 23:36:09 +0300

    first commit

--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -45,7 +45,48 @@ class TelegramController extends AbstractController
         $botman = BotManFactory::create($config, null);
 
         // Telegram Bot
+$botman->hears('/start', function (BotMan $bot) {
+    $bot->reply('Commands:');
+    $bot->reply('Hello');
+    $bot->reply('/what_is_my_id');
+    $bot->reply('/weather');
+    $bot->reply('/random_image');
+});
 
+$botman->hears('hello', function (BotMan $bot) {
+    $name = $bot->getUser()->getFirstName() . ' ' . $bot->getUser()->getLastName();
+    $name = trim($name);
+    $bot->reply('Hello ' . $name);
+});
+
+$botman->hears('/what_is_my_id', function (BotMan $bot) {
+    $id = $bot->getUser()->getId();
+    $bot->reply($id);
+});
+
+/*
+$botman->hears('/weather', function ($bot) {
+    $bot->startConversation(new WeatherConversation());
+});
+*/
+
+$botman->hears('/random_image', function (BotMan $bot) {
+    $width = mt_rand(800, 1920);
+    $height = mt_rand(600, 1080);
+    $attachment = new \BotMan\BotMan\Messages\Attachments\Image(
+        'https://theoldreader.com/kittens/' . $width . '/' . $height . '/'
+    );
+    $message = \BotMan\BotMan\Messages\Outgoing\OutgoingMessage::create('random image')
+        ->withAttachment($attachment);
+    $bot->reply($message);
+});
+
+$botman->receivesImages(function ($bot, $images) {
+    foreach ($images as $image) {
+        $url = $image->getUrl();
+        $bot->reply($url);
+    }
+});
 
 
         // Start listening
